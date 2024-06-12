ab90932508a8bd6514ec6f9e12acd615faf6d994
Thejandeera <thejan1010@gmail.com>
2024-06-13 00:50:44 +0530

    First commit

---
+++ intro.ts
@@ -0,0 +1,4 @@
+let user = {name:"Thejan", age : 21}
+
+console.log("Thejan");
+console.log(user.name);
---
+++ variableMe.ts
@@ -0,0 +1,27 @@
+let greetings:string = "Hello Thejan ";
+
+greetings.toLocaleLowerCase(); 
+console.log(greetings);
+
+//number
+
+let userId:number = 200228400890;
+userId.toFixed()
+
+//boolean
+
+let isLoggedIn : boolean = false;
+
+//any
+
+let hero;
+
+function getHero(){
+    return "thor"
+}
+
+hero = getHero()
+
+
+
+export{}
