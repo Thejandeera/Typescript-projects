418ab60050087b68880f5c4870ceeb394c200dec
Thejandeera <thejan1010@gmail.com>
2024-06-13 11:12:21 +0530

    third commit

---
+++ myFunctions.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signupUser(name, email, ispaid) {
+}
+function loginUser(name, email, yesorno) {
+    if (yesorno === void 0) { yesorno = false; }
+}
+addTwo(5);
+getUpper("thejan");
+signupUser("Thejan", "thejan1010@gmail.com", true);
---
+++ myFunctions.ts
@@ -0,0 +1,20 @@
+function addTwo(num : number){
+    
+    return num + 2
+}
+
+function getUpper(val : string){
+    return val.toUpperCase()
+}
+
+function signupUser(name : string , email : string , ispaid : boolean){
+
+}
+
+function loginUser(name:string , email : string , yesorno : boolean = false) :void {}
+
+let number2 : number = addTwo(5)
+let name2 : string = getUpper("thejan")
+signupUser("Thejan","thejan1010@gmail.com",true)
+
+export{}
