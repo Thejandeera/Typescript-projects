3be1139a0f55c60e69950bc9d6f7fe4f8d5f5247
Thejandeera <thejan1010@gmail.com>
2024-06-13 12:59:47 +0530

    fourth commit

--- myFunctions.ts
+++ myFunctions.ts
@@ -17,4 +17,34 @@ let number2 : number = addTwo(5)
 let name2 : string = getUpper("thejan")
 signupUser("Thejan","thejan1010@gmail.com",true)
 
+
+function getvalue(myval:number) {
+    if (myval>5){
+        return true
+    }
+    return "200 ok"
+}
+
+const getHello = (S:string):string => {
+    return ""
+}
+
+const heroes = ["thor", "superman","ironman"]
+
+heroes.map(hero => {
+    return `hero is ${hero}`
+})
+
+
+function consoleError(errmsg:string):void{
+    console.log(errmsg)
+}
+
+handleError("Thejandeera")
+
+//if we used nevwe , and if we passed a value for that function , it will show an error msg on the ui
+function handleError(errmsg:string):never{
+    throw new Error(errmsg);
+}
+
 export{}
---
+++ myObjects.ts
@@ -0,0 +1,34 @@
+const user = {
+    name : "thejan",
+    email : "thejan1010@gmail.com",
+    isActive : true
+}
+
+function createUser({name:string ,ispaid : boolean}){}
+
+
+let newuser =  ({name : "thejan",ispaid : false , email:"thejan1010@gmail.com"})
+
+function createCourse():{name: string , price : number}{
+    return {name : "reactjs", price : 399}
+}
+createUser(newuser)
+
+
+//Type Aliases
+
+type user ={
+    name : string;
+    email : string;
+    isActive : boolean
+}
+
+type myString = string
+
+function createUser2(user : user):user{
+    return {name:"",email: ",",isActive:true}
+}
+
+createUser2({name:"",email: ",",isActive:true})
+
+export{}
